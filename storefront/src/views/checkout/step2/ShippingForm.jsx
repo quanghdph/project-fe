b29368c903b7a85a865ee4e6c6e6a7566484c502
quanghdph <quanghdph08157@fpt.edu.vn>
2024-01-17 02:40:16 +0700

    update online

--- storefront/src/views/checkout/step2/ShippingForm.jsx
+++ storefront/src/views/checkout/step2/ShippingForm.jsx
@@ -1,10 +1,15 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import { CustomInput, CustomMobileInput } from "@/components/formik";
-import { Flex, Text } from "@chakra-ui/react";
-import { Col, Form, Input, Row, Select } from "antd";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { Button, Col, Form, Input, Radio, Row, Select } from "antd";
+import TextArea from "antd/lib/input/TextArea";
+import axios from "axios";
 import { Field, Formik, useFormikContext } from "formik";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useForm, useWatch } from "react-hook-form";
 // import * as Yup from "yup";
+import "antd/dist/antd.css";
+import "antd-button-color/dist/css/style.css";
 
 const ShippingForm = () => {
   const { values } = useFormikContext();
@@ -18,157 +23,259 @@ const ShippingForm = () => {
   //   note: Yup.string().required("Điền ghi chú"),
   // });
 
+  const [provinceOption, setProvinceOption] = useState();
+  const [districtOption, setDistrictOption] = useState();
+  const [wardOption, setWardOption] = useState();
+  const [districtVisible, setDistrictVisible] = useState(true);
+  const [wardVisible, setWardVisible] = useState(true);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    trigger,
+    control,
+    getValues,
+    reset,
+    formState: { errors, isValid },
+  } = useForm();
+
+  const watchProvince = useWatch({ control, name: "province" });
+  const watchDistrict = useWatch({ control, name: "district" });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios
+          .get(
+            "https://online-gateway.ghn.vn/shiip/public-api/master-data/province",
+            {
+              headers: {
+                token: "264902db-af84-11ee-8586-12380ed2f541",
+              },
+            }
+          )
+          .then((response) => {
+            const listOption =
+              response.data.data.map((item) => ({
+                value: item.ProvinceID,
+                label: item.ProvinceName,
+              }));
+            listOption && setProvinceOption(listOption);
+            setDistrictVisible(false);
+          });
+          return response
+      } catch (error) {
+        console.error("Error fetching province data:", error);
+      }
+    };
+
+    fetchData();
+
+    return () => {};
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async (province_id) => {
+      try {
+        const response = await axios.get(
+          "https://online-gateway.ghn.vn/shiip/public-api/master-data/ward",
+          {
+            params: { province_id },
+            headers: {
+              token: "264902db-af84-11ee-8586-12380ed2f541",
+            },
+          }
+        ).then((response) => {
+          const listOption = response.data.data.map((item) => ({
+            value: item.DistrictID,
+            label: item.DistrictName,
+          }));
+          listOption && setDistrictOption(listOption);
+          setWardVisible(false);
+        });
+     
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        // Handle errors as needed
+      }
+    };
+
+    watchProvince && fetchData(watchProvince);
+
+    return () => {};
+  }, [watchProvince]);
+
+  useEffect(() => {
+    const fetchData = async (district_id) => {
+      try {
+        const response = await axios.get(
+          "https://online-gateway.ghn.vn/shiip/public-api/master-data/ward",
+          {
+            params: { district_id },
+            headers: {
+              token: "264902db-af84-11ee-8586-12380ed2f541",
+            },
+          }
+        ).then((response) => {
+          const listOption = response.data.data.map((item) => ({
+            value: item.WardCode,
+            label: item.WardName,
+          }));
+          listOption && setWardOption(listOption);
+          
+        });
+       return response;
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        // Handle errors as needed
+      }
+    };
+
+    watchDistrict && fetchData(watchDistrict);
+
+    return () => {};
+  }, [watchDistrict]);
+
   return (
     <div className="checkout-shipping-wrapper">
       <div className="checkout-shipping-form">
-      <Form
-            layout="vertical"
-            autoComplete="off"
-            onFinish={handleSubmit(onSubmit)}
-          >
-            <Row gutter={[20, 16]}>
-              <Col span={12}>
-                <Flex gap={5}>
-                  <Form.Item
-                    name="fullname"
-                    label="Họ và tên"
-                    labelCol={{ span: 24 }}
-                    validateStatus={errors.fullname ? "error" : ""}
-                    help={errors.fullname ? errors.fullname.message : ""}
-                    style={{ width: "50%" }}
-                  >
-                    <Input
-                      {...register("fullname", {
-                        required: "Vui lòng nhập họ và tên",
-                      })}
-                      onChange={(e) => setValue("fullname", e.target.value)}
-                      placeholder="Nguyễn Văn A"
-                    />
-                  </Form.Item>
-
-                  <Form.Item
-                    name="phoneNumber"
-                    label="Số điện thoại"
-                    labelCol={{ span: 24 }}
-                    validateStatus={errors.phoneNumber ? "error" : ""}
-                    help={errors.phoneNumber ? errors.phoneNumber.message : ""}
-                    style={{ width: "50%" }}
-                  >
-                    <Input
-                      {...register("phoneNumber", {
-                        required: "Vui lòng nhập số điện thoại",
-                      })}
-                      onChange={(e) => setValue("phoneNumber", e.target.value)}
-                      placeholder="0932384746"
-                    />
-                  </Form.Item>
-                </Flex>
-                <Flex gap={3}>
-                  <Form.Item
-                    name="province"
-                    label={
-                      <span>
-                        Tỉnh/thành phố
-                        {errors.province && (
-                          <span style={{ color: "red" }}>*</span>
-                        )}
-                      </span>
-                    }
-                    labelCol={{ span: 24 }}
-                    validateStatus={errors.province ? "error" : ""}
-                    help={errors.province ? errors.province.message : ""}
-                    style={{ width: "31.7%" }}
-                  >
-                    <Select
-                      placeholder="Chọn tỉnh thành phố"
-                      {...register("province", {
-                        required: "Vui lòng chọn tỉnh thành phố",
-                      })}
-                      onChange={(value: any) => setValue("province", value)}
-                      options={provinceOption}
-                    />
-                  </Form.Item>
-                  <Form.Item
-                    name="district"
-                    label={
-                      <span>
-                        Quận/Huyện
-                        {errors.district && (
-                          <span style={{ color: "red" }}>*</span>
-                        )}
-                      </span>
-                    }
-                    labelCol={{ span: 24 }}
-                    validateStatus={errors.district ? "error" : ""}
-                    help={errors.district ? errors.district.message : ""}
-                    style={{ width: "31.7%" }}
-                  >
-                    <Select
-                      placeholder="Chọn quận huyện"
-                      {...register("district", {
-                        required: "Vui lòng chọn quận huyện",
-                      })}
-                      disabled={districtVisible}
-                      onChange={(value: any) => setValue("district", value)}
-                      options={districtOption}
-                    />
-                  </Form.Item>
-                  <Form.Item
-                    name="ward"
-                    label={
-                      <span>
-                        Xã/Phường/Thị Trấn
-                        {errors.ward && <span style={{ color: "red" }}>*</span>}
-                      </span>
-                    }
-                    labelCol={{ span: 24 }}
-                    validateStatus={errors.ward ? "error" : ""}
-                   
-                    help={errors.ward ? errors.ward.message : ""}
-                    style={{ width: "31.7%" }}
-                  >
-                    <Select
-                      placeholder="Chọn xã phường thị trân"
-                      {...register("ward", {
-                        required: "Vui lòng chọn xã phường thị trân",
-                      })}
-                      onChange={(value: any) => setValue("ward", value)}
-                      disabled={wardVisible}
-                      options={wardOption}
-                    />
-                  </Form.Item>
-                </Flex>
+        <Form
+          layout="vertical"
+          autoComplete="off"
+          // onFinish={handleSubmit(onSubmit)}
+        >
+          <Row gutter={[20, 16]}>
+            <Col span={12}>
+              <Flex gap={5}>
                 <Form.Item
-                  name="address"
-                  label="Địa chỉ cụ thể"
+                  name="fullname"
+                  label="Họ và tên"
                   labelCol={{ span: 24 }}
-                  validateStatus={errors.address ? "error" : ""}
-                  help={errors.address ? errors.address.message : ""}
+                  // validateStatus={errors.fullname ? "error" : ""}
+                  help={errors.fullname ? errors.fullname.message : ""}
                   style={{ width: "50%" }}
                 >
                   <Input
-                    {...register("address", {
-                      required: "Vui lòng nhập địa chỉ cụ thể",
+                    {...register("fullname", {
+                      required: "Vui lòng nhập họ và tên",
                     })}
-                    onChange={(e) => setValue("address", e.target.value)}
-                    placeholder="Số nhà 20,.."
+                    onChange={(e) => setValue("fullname", e.target.value)}
+                    placeholder="Nguyễn Văn A"
                   />
                 </Form.Item>
-              </Col>
-              <Col span={12}>
-                <Flex justifyContent={"space-between"}>
-                  <Text>Tổng tiền:</Text>
-                  <Text>{formatPriceVND(Number(totalAmount))}</Text>
-                </Flex>
-                <Box mb={4}>
-                  <Text>Tiền khách đưa:</Text>
-                  <InputNumber prefix="VND" style={{ width: "100%" }} />
-                </Box>
-                <Flex justifyContent={"space-between"}>
-                  <Text>Tiền dư:</Text>
-                  <Text>Tự đi mà tính</Text>
-                </Flex>
-                <Flex direction={"column"} justifyContent={"space-between"}>
+
+                <Form.Item
+                  name="phoneNumber"
+                  label="Số điện thoại"
+                  labelCol={{ span: 24 }}
+                  validateStatus={errors.phoneNumber ? "error" : ""}
+                  help={errors.phoneNumber ? errors.phoneNumber.message : ""}
+                  style={{ width: "50%" }}
+                >
+                  <Input
+                    {...register("phoneNumber", {
+                      required: "Vui lòng nhập số điện thoại",
+                    })}
+                    onChange={(e) => setValue("phoneNumber", e.target.value)}
+                    placeholder="0932384746"
+                  />
+                </Form.Item>
+              </Flex>
+              <Flex gap={3}>
+                <Form.Item
+                  name="province"
+                  label={
+                    <span>
+                      Tỉnh/thành phố
+                      {errors.province && (
+                        <span style={{ color: "red" }}>*</span>
+                      )}
+                    </span>
+                  }
+                  labelCol={{ span: 24 }}
+                  validateStatus={errors.province ? "error" : ""}
+                  help={errors.province ? errors.province.message : ""}
+                  style={{ width: "31.7%" }}
+                >
+                  <Select
+                    placeholder="Chọn tỉnh thành phố"
+                    {...register("province", {
+                      required: "Vui lòng chọn tỉnh thành phố",
+                    })}
+                    onChange={(value) => setValue("province", value)}
+                    options={provinceOption}
+                  />
+                </Form.Item>
+                <Form.Item
+                  name="district"
+                  label={
+                    <span>
+                      Quận/Huyện
+                      {errors.district && (
+                        <span style={{ color: "red" }}>*</span>
+                      )}
+                    </span>
+                  }
+                  labelCol={{ span: 24 }}
+                  validateStatus={errors.district ? "error" : ""}
+                  help={errors.district ? errors.district.message : ""}
+                  style={{ width: "31.7%" }}
+                >
+                  <Select
+                    placeholder="Chọn quận huyện"
+                    {...register("district", {
+                      required: "Vui lòng chọn quận huyện",
+                    })}
+                    disabled={districtVisible}
+                    onChange={(value) => setValue("district", value)}
+                    options={districtOption}
+                  />
+                </Form.Item>
+                <Form.Item
+                  name="ward"
+                  label={
+                    <span>
+                      Xã/Phường/Thị Trấn
+                      {errors.ward && <span style={{ color: "red" }}>*</span>}
+                    </span>
+                  }
+                  labelCol={{ span: 24 }}
+                  validateStatus={errors.ward ? "error" : ""}
+                  help={errors.ward ? errors.ward.message : ""}
+                  style={{ width: "31.7%" }}
+                >
+                  <Select
+                    placeholder="Chọn xã phường thị trân"
+                    {...register("ward", {
+                      required: "Vui lòng chọn xã phường thị trân",
+                    })}
+                    onChange={(value) => setValue("ward", value)}
+                    disabled={wardVisible}
+                    options={wardOption}
+                  />
+                </Form.Item>
+              </Flex>
+              <Form.Item
+                name="address"
+                label="Địa chỉ cụ thể"
+                labelCol={{ span: 24 }}
+                validateStatus={errors.address ? "error" : ""}
+                help={errors.address ? errors.address.message : ""}
+                style={{ width: "50%" }}
+              >
+                <Input
+                  {...register("address", {
+                    required: "Vui lòng nhập địa chỉ cụ thể",
+                  })}
+                  onChange={(e) => setValue("address", e.target.value)}
+                  placeholder="Số nhà 20,.."
+                />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              {/* <Flex direction={"column"} justifyContent={"space-between"}>
                   <Text>Chọn phương thức thanh toán</Text>
                   <Form.Item name="paymentMethod">
                     <Radio.Group
@@ -199,34 +306,34 @@ const ShippingForm = () => {
                       </Radio.Button>
                     </Radio.Group>
                   </Form.Item>
-                </Flex>
-                <Box mb={2}>
-                  <Form.Item
-                    name="note"
-                    label="Ghi chú"
-                    labelCol={{ span: 24 }}
-                    rules={[{ required: true, message: "Điền ghi chú" }]}
-                  >
-                    <TextArea
-                      // {...register("note")}
-                      // onChange={onNoteChange}
-                      rows={4}
-                      placeholder="Nhập ghi chú"
-                    />
-                  </Form.Item>
-                </Box>
-
-                <Button
-                  // htmlType="submit"
-                  type="primary"
-                  // loading={selloff?.create?.loading}
-                  onClick={showBillModal}
+                </Flex> */}
+              <Box mb={2}>
+                <Form.Item
+                  name="note"
+                  label="Ghi chú"
+                  labelCol={{ span: 24 }}
+                  rules={[{ required: true, message: "Điền ghi chú" }]}
                 >
-                  Hoàn thành
-                </Button>
-              </Col>
-            </Row>
-          </Form>
+                  <TextArea
+                    // {...register("note")}
+                    // onChange={onNoteChange}
+                    rows={4}
+                    placeholder="Nhập ghi chú"
+                  />
+                </Form.Item>
+              </Box>
+
+              <Button
+                // htmlType="submit"
+                type="primary"
+                // loading={selloff?.create?.loading}
+                // onClick={showBillModal}
+              >
+                Hoàn thành
+              </Button>
+            </Col>
+          </Row>
+        </Form>
       </div>
     </div>
   );
